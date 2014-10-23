96e3cacb09301969c5066e465f1a1853fb287bed
Naman Agarwal <namananaman@gmail.com>
2014-10-22 21:33:11 -0400

    added iteration so we could get more than 100 tweets

--- search-test.py
+++ search-test.py
@@ -8,7 +8,7 @@ parser.add_argument("--out_file", help="the file to output results", default='da
 parser.add_argument("--include_entities", help="whether to include entitites", default=True)
 parser.add_argument("--key_file", help="where to read in the keys from, which should be a file with four lines, containing the consumer_key, consumer_secret, access_token, and access_token secret (or just change the values in the script)")
 parser.add_argument("--search_terms", help="search terms for the search", nargs='+', required=True)
-parser.add_argument("--num_results", help="number of results you want", default=100)
+parser.add_argument("--num_results", help="number of results you want", type=int, default=100)
 
 args = parser.parse_args()
 print "Search Terms: ", args.search_terms
@@ -27,9 +27,15 @@ else:
 tso = TwitterSearchOrder() # create a TwitterSearchOrder object
 tso.setKeywords(args.search_terms) # let's define all words we would like to have a look for
 tso.setLanguage('en') # english
-tso.setCount(args.num_results)
+tso.setCount(min(args.num_results, 100))
 tso.setIncludeEntities(args.include_entities) # and give us all those entity information
 
+iters = 1
+if args.num_results > 100:
+    args.num_results += 99
+    iters = args.num_results / 100
+
+print(tso.createSearchURL());
 start_point = 0
 if os.path.isfile(args.out_file):
     with file(args.out_file) as fout:
@@ -58,27 +64,32 @@ latest_id = 0
 num_tweets = 0
 next_max_id = 0
 
-# first query the Twitter API
-response = ts.searchTweets(tso)
+for i in range(iters):
+    # first query the Twitter API
+    response = ts.searchTweets(tso)
 
-# print rate limiting status
-print "Current api calls remaining: %s" % ts.getMetadata()['x-rate-limit-remaining']
+    # print rate limiting status
+    print "Current api calls remaining: %s" % ts.getMetadata()['x-rate-limit-remaining']
+    old_num_tweets = num_tweets
+    # check all tweets according to their ID
+    for tweet in response['content']['statuses']:
+        num_tweets += 1
+        tweet_id = tweet['id']
 
-# check all tweets according to their ID
-for tweet in response['content']['statuses']:
-    num_tweets += 1
-    tweet_id = tweet['id']
+        print("Seen tweet with ID %i" % tweet_id)
+        with open(args.out_file,'a') as fout:
+            pprint.pprint(tweet,stream=fout)
 
-    print("Seen tweet with ID %i" % tweet_id)
-    with open(args.out_file,'a') as fout:
-        pprint.pprint(tweet,stream=fout)
-
-    if (latest_id == 0):
-        latest_id = tweet_id
-    # current ID is lower than current next_max_id?
-    if (tweet_id < next_max_id) or (next_max_id == 0):
-        next_max_id = tweet_id
-
+        if (latest_id == 0):
+            latest_id = tweet_id
+        # current ID is lower than current next_max_id?
+        if (tweet_id < next_max_id) or (next_max_id == 0):
+            next_max_id = tweet_id
+            next_max_id -= 1
+    tso.setMaxID(next_max_id)
+    if (num_tweets == old_num_tweets):
+        #no tweets returned... i could also check if the length is equal to 0
+        break
 
 print "Number of tweets seen: %i" % num_tweets
 if (num_tweets > 0):
